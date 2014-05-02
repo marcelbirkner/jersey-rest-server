2c0af42e0e1f245717bcc97a8a1982916a9524d8
marcelbirkner <marcel.birkner@codecentric.de>
2014-05-02 17:31:57 +0200

    MB: added package info

---
+++ src/main/java/de/mb/rest/service/customer/package-info.java
@@ -0,0 +1,4 @@
+@XmlSchema(namespace = "https://github.com/marcelbirkner/jersey-rest-server/customer")
+package de.mb.rest.service.customer;
+
+import javax.xml.bind.annotation.XmlSchema;
---
+++ src/main/java/de/mb/rest/service/ping/package-info.java
@@ -0,0 +1,4 @@
+@XmlSchema(namespace = "https://github.com/marcelbirkner/jersey-rest-server/ping")
+package de.mb.rest.service.ping;
+
+import javax.xml.bind.annotation.XmlSchema;
